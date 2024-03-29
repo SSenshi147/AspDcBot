839c1ab219df209eb8d5bb451cc8497d79adc32e
SSenshi147 <vojtko.marton@gmail.com>
2024-03-29 17:55:40 +0100

    ping timing

--- DonDumbledore.Logic/SlashCommands/PingCommand.cs
+++ DonDumbledore.Logic/SlashCommands/PingCommand.cs
@@ -1,5 +1,6 @@
 ﻿using Discord;
 using Discord.WebSocket;
+using Hangfire;
 
 namespace DonDumbledore.Logic.Requests;
 
@@ -24,6 +25,22 @@ public class PingCommand : IDonCommand
 
     public async Task Handle(SocketSlashCommand arg)
     {
-        await arg.RespondAsync(text: $"pong {arg.Data.Options.FirstOrDefault()?.Value}");
+        await arg.RespondAsync(text: "received");
+        
+        var neved = arg.Data.Options.FirstOrDefault()?.Value;
+        
+        await arg.FollowupAsync(text: $"pong {neved}");
+
+        if (neved is not string asd) return;
+        var timing = DateTime.TryParse(asd, out var time);
+        if (!timing) return;
+        Console.WriteLine("timing ok!");
+        await arg.FollowupAsync(text: "processed");
+        BackgroundJob.Schedule(() => Anyad(arg), DateTime.Parse(asd));
+    }
+
+    public async Task Anyad(SocketSlashCommand arg)
+    {
+        await arg.FollowupAsync(text: "asd");
     }
 }
