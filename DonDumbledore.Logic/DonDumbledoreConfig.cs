83a735b0b8e118250eeed402d4e18fa86e5276ad
SSenshi147 <vojtko.marton@gmail.com>
2024-03-29 23:06:33 +0100

    command registration

--- DonDumbledore.Logic/DonDumbledoreConfig.cs
+++ DonDumbledore.Logic/DonDumbledoreConfig.cs
@@ -2,9 +2,10 @@
 
 public sealed class DonDumbledoreConfig
 {
+    public bool IsProductionEnvironment { get; set; }
+    public bool RegisterNewCommands { get; set; }
+
     public string SqliteConnectionString { get; set; }
     public string HangfireSqliteConnectionString { get; set; }
     public string BotToken { get; set; }
-    public bool DeleteAndReRegisterGuildCommands { get; set; }
-    public bool DeleteAndReRegisterGlobalCommands { get; set; }
 }
